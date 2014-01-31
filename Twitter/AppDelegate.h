e46f475156378909347ecc9298ef3d152efd101f
Alberto Campos <alberto_campos@symantec.com>
2014-01-31 12:56:34 -0800

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Alberto Campos on 1/31/14.
+//  Copyright (c) 2014 Alberto Campos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
