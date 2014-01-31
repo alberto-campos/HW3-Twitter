679c6bc2458f21bab4044c623211bd877c2c955d
Alberto Campos <alberto_campos@symantec.com>
2014-01-31 15:34:23 -0800

    Initial commit

---
+++ Twitter/CustomCell.h
@@ -0,0 +1,16 @@
+//
+//  CustomCell.h
+//  Twitter
+//
+//  Created by Alberto Campos on 1/31/14.
+//  Copyright (c) 2014 Alberto Campos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *author;
+@property (weak, nonatomic) IBOutlet UILabel *tweet;
+
+
+@end
---
+++ Twitter/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Twitter
+//
+//  Created by Alberto Campos on 1/31/14.
+//  Copyright (c) 2014 Alberto Campos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
